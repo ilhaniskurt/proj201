f2c3a26860f070c77ba5b619e6b510d8ce98aedb
ilhaniskurt <ilhanyavuz.02@hotmail.com>
2021-10-27 21:42:41 +0300

    added more filters for formating courser & deleted excess code

--- beta/logic.py
+++ beta/logic.py
@@ -3,30 +3,29 @@
 #       ilhan.iskurt@sabanciuniv.edu
 #
 
+# Local Modules
 import parser
 
-def displayCourses(array):
-    for course, conditions in array.items():
+def displayDicts(dict):
+    for course, conditions in dict.items():
         print(f"{course} : {conditions}")
 
-def formatCourses(array):
+# Format catalog into logic statements
+def formatCourses(dict):
     courses = {}
-    for a in array:
+    for a in dict:
         # Strip Unnecessary Words
-        temp = a[2].replace(" ", "").replace("MinimumGradeofD","").replace("level","").replace("Undergraduate","").replace("Masters", "").replace("Doctorate","")
+        temp = a[2].replace(" ", "").replace("MinimumGradeofD","").replace("MinimumGradeofS","").replace("level","").replace("Undergraduate","").replace("Masters", "").replace("Doctorate","").replace(">Pre-requisite","").replace(">Pre-requiste","")
         # Add Logic Operators
         temp = temp.replace("or","|").replace("and","&")
         courses[a[0]] = temp
     return courses
 
 # Find Imminent Dependencies
-def findDependencies(array):
+def findDependencies(dict):
     dependencies = {}
-    for course ,conditions in array.items():
-        if "&" in conditions:
-            if "|" not in conditions:
-                dependencies[course] = conditions
-        if conditions.find("&") != -1 and conditions.find("|") != -1 and conditions.find("N/A")!= -1:
+    for course ,conditions in dict.items():
+        if conditions.find("&") != -1 and conditions.find("|") == -1 and conditions.find("N/A") == -1:
             dependencies[course] = conditions
     return dependencies
 
@@ -34,7 +33,9 @@ def findDependencies(array):
 def main():
     output = parser.getPickle(parser.PICKLE_NAME)
     courses = formatCourses(output)
-    displayCourses(findDependencies(courses))
+
+    dependencies = findDependencies(courses)
+    displayDicts(dependencies)
 
 if __name__ == "__main__":
     main()
