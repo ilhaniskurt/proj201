79c0123a5bd4c3123784a2128cb56a038d25c0b5
ilhaniskurt <ilhanyavuz.02@hotmail.com>
2021-11-05 21:45:01 +0300

    working on fixing ghosts

--- beta/logic.py
+++ beta/logic.py
@@ -21,11 +21,32 @@ def fixDuplicates(courses:dict, duplicates:dict):
                     fixedDict[course] = conditions[:res[0]] + conditions[res[1]:]
     return fixedDict
 
-def fixGhosts():
-    pass
+# Remove ghost courses out of the prerequisites (Not finisihed)
+def fixGhosts(courses:dict, ghosts:dict):
+    fixedDict = courses
+    for course, conditions in courses.items():
+        if course in ghosts.keys():
+            for ghost in ghosts[course]:
+                # If the only condition is a ghost
+                if conditions == ghost:
+                    fixedDict[course] = "N/A"
+                    continue
+                i = conditions.find(ghost)
+                # If ghost course is isolated in parenthesis
+                if ghost[i-1] == "(" and ghost[i+len(ghost)+1] == ")": 
+                    fixedDict[course] = conditions[:i-2] + conditions[i+2:]
+                    continue
+                for operator in ["&","|"]:
+                    # If ghost course is surrounded with same logic operators
+                    if ghost[i-1] == operator and ghost[i+len(ghost)+1] == operator:
+                        fixedDict[course] = conditions[:i-2] + conditions[i+1:]
+                        continue
+        # NOTE Remember to check for logic operators that does not have courses on both sides (parenthesis does not count) and remove them
+    return fixedDict
 
-def fixLoops():
-    pass
+def fixLoops(courses:dict, loops:dict):
+    fixedDict = courses
+    return fixedDict
 
 # Entry Point
 def main():
