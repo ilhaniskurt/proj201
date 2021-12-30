7edaa7e358ab07e1c86e022374c502703eac2659
ilhaniskurt <85507446+ilhaniskurt@users.noreply.github.com>
2021-12-30 16:39:29 +0300

    Update root.py

--- scripts/root.py
+++ scripts/root.py
@@ -19,16 +19,16 @@ def main():
     # Start Runtime
     start = perf_counter()
     
-    # parseCatalog() # Parse the html and save it as "course_list"
-    catalog_list = getPickle("catalog_list") # Parsed html output from the function: "parseCatalog"
-    catalog_dict, syntax_errors = formatCourses(catalog_list) # Format courses into logic statements
+    parseCatalog() # Parse the html and save it as "course_list". You can comment out this code if you ran it once.
+    catalog_list = getPickle("catalog_list") # Parsed html output from the function: "parseCatalog".
+    catalog_dict, syntax_errors = formatCourses(catalog_list) # Format courses into logic statements.
     catalog_dict2 = fixLoops(catalog_dict) # Fix courses that require themselves without recursive search.
-    catalog_dict_with_ghosts = fixRedundancies(catalog_dict2) # Remove redundant prerequisites
-    catalog_dict_without_ghosts, ghost_errors = fixGhosts(catalog_dict_with_ghosts) # Solve courses involving ghost prerequisites
+    catalog_dict_with_ghosts = fixRedundancies(catalog_dict2) # Remove redundant prerequisites.
+    catalog_dict_without_ghosts, ghost_errors = fixGhosts(catalog_dict_with_ghosts) # Solve courses involving ghost prerequisites.
     
     
     # Display the catalog dictionary to the terminal
-    displayDicts(catalog_dict_with_ghosts) # Replace with the catalog dictionary you want to display
+    displayDicts(catalog_dict_with_ghosts) # Replace with the catalog dictionary you want to display.
     
     
     # End Runtime
